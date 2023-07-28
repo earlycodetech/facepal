646ece6caa491fb398a152127a3febd919eb109c
unknown <clemnanzzy@gmail.com>
2023-07-28 16:32:40 +0100

    commit recently

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
-
+import { Footer } from './components/footer';
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
